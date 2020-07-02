fb8061f5494bb25d4b05737bbc36398ded31d99e
Sriverfx <athaxeno@gmail.com>
2020-07-02 22:42:24 +0200

    update classes.js

---
+++ classes.js
@@ -0,0 +1,22 @@
+class Player
+{
+    constructor(name)
+    {
+        this.name = name;
+    }
+}
+
+/*class Animal{
+    constructor()
+    {
+        this.type = "Animal";
+    }
+}*/
+
+// export default {
+//     Player
+// }; */ 
+module.exports = {
+    Player
+    
+};
